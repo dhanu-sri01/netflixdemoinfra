1e354d7018c8facef72d98302212e1e5417d96f7
dhanu-sri01 <dhanusribattula@gmail.com>
2025-02-03 15:16:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "one" {
-  count                    = 4
+  count                    = 5
   ami                      = "ami-0f214d1b3d031dc53"  # AMI ID (make sure it's correct)
   instance_type            = "t2.micro"
   key_name                 = "keys_ec2"
@@ -13,17 +13,7 @@ resource "aws_instance" "one" {
   }
 }
 
-resource "aws_instance" "two" {
-  ami                      = "ami-0f214d1b3d031dc53"
-  instance_type            = "t2.medium"
-  key_name                 = "keys_ec2"
-  vpc_security_group_ids   = ["sg-0cd50267c97e57021"]
-  tags = {
-    Name = "Nexus"
-  }
-}
-
 variable "instance_names" {
-  default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
+  default = ["jenkins","nexus" "AppServer-1", "AppServer-2", "Monitoring server"]
 }
 
